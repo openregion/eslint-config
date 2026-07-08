fcfcd9265733ba9d96f83c5f074039491dff4f3e
Gruven <igruven@gmail.com>
2026-07-08 16:03:31 +0500

    feat: export eslint flat config

--- index.js
+++ index.js
@@ -1,108 +1,110 @@
-module.exports = {
-  "extends": [
-    "airbnb",
-    "airbnb/hooks",
-    "plugin:@typescript-eslint/recommended"
-  ],
-  "globals": {
-    "NODE_ENV": true,
-    "API_URL_ENV": true,
-    "VALIDATION_STATUS": true
-  },
-  "plugins": [
-    "@typescript-eslint",
-    "import"
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": 2021,
-    "sourceType": "module"
-  },
-  "env": {
-    "browser": true
-  },
-  "rules": {
-    "arrow-parens": "warn",
-    "max-len": "off",
-    "no-console": 1,
-    "no-trailing-spaces": 0,
-    "no-mixed-operators": 0,
-    "no-underscore-dangle": 0,
-    "no-shadow": 0,
-    "dot-notation": 0,
-    "jsx-a11y/anchor-is-valid": [
-      "error",
-      {
-        "components": [
-          "NavLink",
-          "Link"
-        ],
-        "specialLink": [
-          "to"
-        ]
-      }
-    ],
-    "import/no-unresolved": 0,
-    "import/extensions": 0,
-    "implicit-arrow-linebreak": "off",
-    "import/prefer-default-export": 0,
-    "react/jsx-filename-extension": [
-      1,
-      {
-        "extensions": [
-          ".tsx",
-          ".jsx"
-        ]
-      }
-    ],
-    "react/forbid-prop-types": 0,
-    "react/jsx-props-no-spreading": [
-      "off"
-    ],
-    "react/prop-types": [
-      1,
-      {
-        "skipUndeclared": true
-      }
-    ],
-    "react/require-default-props": [
-      1,
-      {
-        "ignoreFunctionalComponents": true
-      }
-    ],
-    "jsx-a11y/click-events-have-key-events": 0,
-    "@typescript-eslint/explicit-function-return-type": [
-      1,
-      {
-        "allowExpressions": true
-      }
-    ],
-    "@typescript-eslint/no-namespace": [
-      1,
-      {
-        "allowDeclarations": true
-      }
-    ],
-    "jsx-a11y/label-has-associated-control": 0,
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": ["error"],
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": ["warn"],
-    "no-duplicate-imports": "off",
-    "import/no-duplicates": "error",
-    "space-before-function-paren": "off",
-    "@typescript-eslint/space-before-function-paren": "off",
-    "function-paren-newline": "off",
-    "react/function-component-definition": "off",
-    "react/react-in-jsx-scope": "off"
-  },
-  "settings": {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"]
-    },
-    "import/resolver": {
-      "typescript": {}
-    }
-  }
+import eslintReact from "@eslint-react/eslint-plugin";
+import js from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin";
+import { defineConfig } from "eslint/config";
+import importX from "eslint-plugin-import-x";
+import reactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { legacyAirbnbRules } from "./rules/legacy-airbnb-rules.js";
+
+const sourceFiles = ["**/*.{js,mjs,cjs,jsx,ts,tsx}"];
+const jsxFiles = ["**/*.{jsx,tsx}"];
+
+const projectGlobals = {
+  NODE_ENV: "readonly",
+  API_URL_ENV: "readonly",
+  VALIDATION_STATUS: "readonly",
 };
+
+const localBaseRules = {
+  ...legacyAirbnbRules,
+  "no-console": "warn",
+  "no-underscore-dangle": "off",
+  "no-shadow": "off",
+  "dot-notation": "off",
+  "no-use-before-define": "off",
+  "no-unused-vars": "off",
+  "no-duplicate-imports": "off",
+  "@stylistic/arrow-parens": "warn",
+  "@stylistic/max-len": "off",
+  "@stylistic/no-trailing-spaces": "off",
+  "@stylistic/no-mixed-operators": "off",
+  "@stylistic/implicit-arrow-linebreak": "off",
+  "@stylistic/space-before-function-paren": "off",
+  "@stylistic/function-paren-newline": "off",
+  "@stylistic/object-property-newline": [
+    "error",
+    {
+      allowAllPropertiesOnSameLine: true,
+    },
+  ],
+  "import-x/no-unresolved": "off",
+  "import-x/extensions": "off",
+  "import-x/prefer-default-export": "off",
+  "import-x/no-cycle": [
+    "error",
+    {
+      ignoreExternal: false,
+      allowUnsafeDynamicCyclicDependency: false,
+    },
+  ],
+  "import-x/no-duplicates": "error",
+};
+
+const localTypeScriptRules = {
+  "@typescript-eslint/explicit-function-return-type": [
+    "warn",
+    {
+      allowExpressions: true,
+    },
+  ],
+  "@typescript-eslint/no-namespace": [
+    "warn",
+    {
+      allowDeclarations: true,
+    },
+  ],
+  "@typescript-eslint/no-use-before-define": "error",
+  "@typescript-eslint/no-unused-vars": "warn",
+};
+
+export default defineConfig([
+  js.configs.recommended,
+  importX.flatConfigs.recommended,
+  {
+    files: sourceFiles,
+    plugins: {
+      "@stylistic": stylistic,
+      "import-x": importX,
+    },
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...projectGlobals,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: localBaseRules,
+  },
+  ...tseslint.configs.recommended,
+  importX.flatConfigs.typescript,
+  {
+    files: sourceFiles,
+    rules: localTypeScriptRules,
+  },
+  {
+    files: jsxFiles,
+    extends: [
+      eslintReact.configs.jsx,
+      eslintReact.configs.dom,
+      reactHooks.configs.flat.recommended,
+    ],
+  },
+]);
